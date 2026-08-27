b75d1cd5d8507e75abb15ac77c6a1b40f3e86792
Ronglong Wu <34789753+wrl297@users.noreply.github.com>
2026-08-27 11:35:14 +0800

    Update about.js

--- src/about.js
+++ src/about.js
@@ -21,11 +21,16 @@ window.aboutData = {
       icon: "fa-solid fa-graduation-cap"
     }
   ],
-  interests: [],
+  interests: [
+    "Reliable Machine Learning Systems",
+    "GPU Cluster Scheduling",
+    "Memory Systems"
+  ],
   paragraphs: [
     "I am Ronglong Wu, a Ph.D. candidate in the School of Informatics at Xiamen University, advised by Prof. Zhirong Shen and co-advised by Prof. Yiming Zhang. I received my B.S. degree from Xiamen University in 2021. From October 2025 to June 2026, I was a Research Intern in the Intelligent Scheduling Group (GPU) at Tencent.",
     // "Currently, I am a Research Intern in the Intelligent Scheduling Group (GPU) at Tencent, a position I have held since Fall 2025.",
-    "My research interests lie in computer systems and architecture, with a particular focus on building efficient and reliable memory systems for emerging computing workloads, especially large language models (LLMs)."
+    "My research interests lie in computer systems and architecture, with a particular focus on building efficient and reliable memory systems for emerging computing workloads, especially large language models (LLMs).",
+    "I am currently seeking industry opportunities in AI infrastructure, particularly in reliable ML systems, GPU cluster scheduling, and memory systems."
   ],
   note: ""
 };
